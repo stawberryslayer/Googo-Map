00f39b8d4634c50f1ea562ed0273f791e56529d1
stawberryslayer <ywang2776@wisc.edu>
2024-10-25 23:57:37 -0500

    Set up map in main activity

--- app/src/main/java/com/cs407/map_application/MainActivity.kt
+++ app/src/main/java/com/cs407/map_application/MainActivity.kt
@@ -5,16 +5,39 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.model.LatLng
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), OnMapReadyCallback {
+    private lateinit var mapService: MapService
+    private lateinit var googleMap: GoogleMap
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        // Initialize the map
+//        val mapFragment = supportFragmentManager
+//            .findFragmentById(R.id.map) as SupportMapFragment
+//        mapFragment.getMapAsync(this)
+    }
+
+    override fun onMapReady(map: GoogleMap) {
+        TODO("Not yet implemented")
+        googleMap = map
+        mapService = MapService(googleMap)
+
+        // Now you can use mapService to interact with the map
+        val defaultLocation = LatLng(37.7749, -122.4194) // Example coordinates
+        mapService.initializeMap(defaultLocation, 10f)
+        mapService.addMarker(defaultLocation, "San Francisco")
     }
 }
