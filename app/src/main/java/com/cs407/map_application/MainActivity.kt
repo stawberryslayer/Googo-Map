5b87e4689596ef75ad0ef031668d5d72fdfbeee7
stawberryslayer <ywang2776@wisc.edu>
2024-11-08 00:20:02 -0600

    Remove Map related content from Main Activity

--- app/src/main/java/com/cs407/map_application/MainActivity.kt
+++ app/src/main/java/com/cs407/map_application/MainActivity.kt
@@ -4,28 +4,35 @@ import android.content.Intent
 import android.os.Bundle
 import android.util.Log
 import android.widget.Button
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
-import com.google.android.gms.maps.GoogleMap
-import com.google.android.gms.maps.OnMapReadyCallback
-import com.google.android.gms.maps.model.LatLng
+import androidx.lifecycle.lifecycleScope
 
-class MainActivity : AppCompatActivity(), OnMapReadyCallback {
+import com.google.android.gms.maps.GoogleMap
+
+class MainActivity : AppCompatActivity() {
     private lateinit var mapService: MapService
     private lateinit var googleMap: GoogleMap
 
+
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
+
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
+
 
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+        mapService = MapService(applicationContext)
             
         // 找到 "Start Plan" 按钮（相当于home page）
         val startPlanButton: Button = findViewById(R.id.button_start_plan)
@@ -39,24 +46,23 @@ class MainActivity : AppCompatActivity(), OnMapReadyCallback {
         }
         startButton.setOnClickListener{
             //send request
+            val origin = "New York, NY"
+            val destination = "Los Angeles, CA"
+
+            // Call the map service to get directions
+            mapService.getDirections(origin, destination, lifecycleScope) { success, message ->
+                if (success) {
+                    Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
+                } else {
+                    Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
+                }
+            }
+
         }
 
-        // Initialize the map
-//        val mapFragment = supportFragmentManager
-//            .findFragmentById(R.id.map) as SupportMapFragment
-//        mapFragment.getMapAsync(this)
     }
 
 
-    override fun onMapReady(map: GoogleMap) {
-        googleMap = map
-        mapService = MapService(googleMap)
-
-        // Now you can use mapService to interact with the map
-        val defaultLocation = LatLng(37.7749, -122.4194) // Example coordinates
-        mapService.initializeMap(defaultLocation, 10f)
-        mapService.addMarker(defaultLocation, "San Francisco")
-    }
 
 
     override fun onStart() {
