1ee183f26de5c5e62c17231bb127c153181de753
julias <shen267@wisc.edu>
2024-12-01 23:12:21 -0600

    Create PlanSegment.kt

---
+++ app/src/main/java/com/cs407/map_application/model/PlanSegment.kt
@@ -0,0 +1,8 @@
+package com.cs407.map_application.model
+
+data class PlanSegment(
+    val startLocation: String,
+    val endLocation: String,
+    val duration: String,
+    val transportMode: String
+)
