2cc90ce8c15fda7bc0d37f3f4bc73fa1cc00a8fc
julias <shen267@wisc.edu>
2024-12-02 21:59:01 -0600

    Update PlanSegment.kt

--- app/src/main/java/com/cs407/map_application/model/PlanSegment.kt
+++ app/src/main/java/com/cs407/map_application/model/PlanSegment.kt
@@ -2,7 +2,6 @@ package com.cs407.map_application.model
 
 data class PlanSegment(
     val startLocation: String,
-    val endLocation: String,
     val duration: String,
     val transportMode: String
 )
